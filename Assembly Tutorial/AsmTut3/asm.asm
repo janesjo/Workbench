e228820b1348c2dee3cd71e4b46c6ca290d28e77
= <=>
2017-06-25 20:21:17 +0200

    Renamed Assembly Tutorials folder

--- Assembly Tutorial/AsmTut3/asm.asm
+++ Assembly Tutorial/AsmTut3/asm.asm
@@ -4,6 +4,10 @@ someFunction proc
 	mov al, 0   ; All 8 lower bits set to zeros
 	mov ax, 0   ; All 16 lower bits set to zeros
 	mov eax, 0  ; All 32 lower bits set to zeros AND 32 higher bits set to zeroes due to a 32 op to a 64 bit register
+	mov r11, -1
+	mov r11b, 0
+	mov r11w, 0
+	mov r11d, 0
 	ret
 someFunction endp
 end
