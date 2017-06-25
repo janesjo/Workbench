0ae6a6bf2d5be56e9de55971089bf5f7779531aa
= <=>
2017-06-25 18:42:15 +0200

    AsmTut2

---
+++ AsmTut1/AsmTut2/asm2.asm
@@ -0,0 +1,9 @@
+.code
+someFunction2 proc
+	push rbx
+	mov bx, -1
+	mov bl, 0
+	pop rbx
+	ret
+someFunction2 endp
+end
---
+++ AsmTut1/AsmTut2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" int someFunction2();
+
+int main() {
+	cout << "The result is: " << someFunction2() << endl;
+	return 0;
+}
